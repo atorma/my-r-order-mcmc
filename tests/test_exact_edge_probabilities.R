f73c0c47d4a62e187e05f8013305b28ab344b1e2
anssi.torma@gmail.com <anssi.torma@gmail.com@4b7b4b30-38c5-59b9-ae8e-f9132cda3d7f>
2012-03-22 15:28:00 +0000

    Testing computing exact edge probabilities. Function returns a result but test fails.

--- tests/test_exact_edge_probabilities.R
+++ tests/test_exact_edge_probabilities.R
@@ -2,40 +2,39 @@ context("Computation of exact edge probabilities")
 
 numNodes <- 3
 maxParents <- 1
-mOrders <- matrix(c(1, 2, 3
-                    2, 1, 3
-                    3, 2, 1
-                    1, 3, 2
-                    2, 3, 1
-                    3, 1, 2))
+# All orderings of nodes 1, 2, 3
+mOrders <- matrix(c(1, 2, 3,
+                    2, 1, 3,
+                    3, 2, 1,
+                    1, 3, 2,
+                    2, 3, 1,
+                    3, 1, 2),
+                    nrow=6, byrow=T)
 
 mockLogLocalStructureScore <- function(node, vParents, vOrder) {
   #cat("Node", node, "parents", vParents, "order", vOrder, fill=T)
   
+  if (length(vParents) == 0) return(-1)
+  
   if (all(vOrder == mOrders[1,])) {
-    
     if (node == 2) {
-      if (length(vParents) == 0) return(-1)
-      else if (vParents == 1) return(-2)    
+      if (vParents == 1) return(-2)    
       
     } else if (node == 3) {
-      if (length(vParents) == 0) return(-1)
-      else if (vParents == 1) return(-2)
-      else if (vParents == 2) return(-3)
+      if (vParents == 1) return(-3)
+      else if (vParents == 2) return(-4)
       
     }
   }
   
   if (all(vOrder == mOrders[2,])) {
     
-    if (node == 1) {
-      if (length(vParents) == 0) return(-1)
-      else if (vParents == 1) return(-2)    
+    if (node == 1) {
+      if (vParents == 2) return(-2)    
       
     } else if (node == 3) {
-      if (length(vParents) == 0) return(-1)
-      else if (vParents == 2) return(-2)
-      else if (vParents == 1) return(-3)
+      if (vParents == 2) return(-3)
+      else if (vParents == 1) return(-4)
       
     }
   }
@@ -43,13 +42,11 @@ mockLogLocalStructureScore <- function(node, vParents, vOrder) {
   if (all(vOrder == mOrders[3,])) {
     
     if (node == 2) {
-      if (length(vParents) == 0) return(-1)
-      else if (vParents == 3) return(-2)    
+      if (vParents == 3) return(-2)    
       
     } else if (node == 1) {
-      if (length(vParents) == 0) return(-1)
-      else if (vParents == 3 return(-2)
-      else if (vParents == 2) return(-3)
+      if (vParents == 3) return(-3)
+      else if (vParents == 2) return(-4)
       
     }
   }
@@ -57,13 +54,11 @@ mockLogLocalStructureScore <- function(node, vParents, vOrder) {
   if (all(vOrder == mOrders[4,])) {
     
     if (node == 3) {
-      if (length(vParents) == 0) return(-1)
-      else if (vParents == 1) return(-2)    
+      if (vParents == 1) return(-2)    
       
     } else if (node == 2) {
-      if (length(vParents) == 0) return(-1)
-      else if (vParents == 1) return(-2)
-      else if (vParents == 3) return(-3)
+      if (vParents == 1) return(-3)
+      else if (vParents == 3) return(-4)
       
     }
   }
@@ -71,13 +66,11 @@ mockLogLocalStructureScore <- function(node, vParents, vOrder) {
   if (all(vOrder == mOrders[5,])) {
     
     if (node == 3) {
-      if (length(vParents) == 0) return(-1)
-      else if (vParents == 2) return(-2)    
+      if (vParents == 2) return(-2)    
       
     } else if (node == 1) {
-      if (length(vParents) == 0) return(-1)
-      else if (vParents == 2) return(-2)
-      else if (vParents == 3) return(-3)
+      if (vParents == 2) return(-3)
+      else if (vParents == 3) return(-4)
       
     }
   }
@@ -85,16 +78,43 @@ mockLogLocalStructureScore <- function(node, vParents, vOrder) {
   if (all(vOrder == mOrders[6,])) {
     
     if (node == 1) {
-      if (length(vParents) == 0) return(-1)
-      else if (vParents == 3) return(-2)    
+      if (vParents == 3) return(-2)    
       
     } else if (node == 2) {
-      if (length(vParents) == 0) return(-1)
-      else if (vParents == 3) return(-2)
-      else if (vParents == 1) return(-3)
+      if (vParents == 3) return(-3)
+      else if (vParents == 1) return(-4)
       
     }
   }
   
   stop("Invalid node, order, or parent set")
 }
+
+# all orders have the same score
+logOrderScore <- log( sum( 3*exp(-1) + exp(-2) + exp(-3) + exp(-4) ) )
+# all orders have the same probability after normalization
+orderProb <- exp(logOrderScore - log(6*exp(logOrderScore)) )
+
+# probability of edge from position 1 to position 2 always same (nodes differ between orders)
+probEdge1to2 <- exp(-2 - logOrderScore)
+# probability of edge from position 1 to position 3 always same (nodes differ between orders)
+probEdge1to3 <- exp(-3 - logOrderScore)
+# probability of edge from position 2 to position 3 always same (nodes differ between orders)
+probEdge2to3 <- exp(-4 - logOrderScore)
+
+# All edges appear in three different orders, xy*, x*y, *xy
+# Then P(edge | D) = sum( P(edge | D)P(< | D), <) is 
+edgeProb <- orderProb*sum(c(probEdge1to2, probEdge1to3, probEdge2to3))
+mExactEdgeProbs <- matrix(edgeProb, nrow=numNodes, ncol=numNodes)
+# Self-refering edges obviously impossible
+mExactEdgeProbs[1,1] <- 0
+mExactEdgeProbs[2,2] <- 0
+mExactEdgeProbs[3,3] <- 0
+print(mExactEdgeProbs)
+
+
+mComputedEdgeProbs <- getExactEdgeProbabilities(numNodes, maxParents, mockLogLocalStructureScore)
+print(mComputedEdgeProbs)
+test_that("Exact edge probabilitites computed correctly when all edges have the same probability", {
+  expect_that(mComputedEdgeProbs, equals(mExactEdgeProbs))
+})
