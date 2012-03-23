03553e566673baf775200d7edc1c27e4b30e0142
anssi.torma@gmail.com <anssi.torma@gmail.com@4b7b4b30-38c5-59b9-ae8e-f9132cda3d7f>
2012-03-23 09:08:38 +0000

    MCMC runs of problems with 25 nodes and 2500 samples. Two runs each of a known artifical model and the unknown model.

--- create_artificial_model.R
+++ create_artificial_model.R
@@ -1,6 +1,6 @@
-numNodes <- 10
+numNodes <- 25
 cardinalities <- rep(3, numNodes)
 maxParents <- 3
 mAdj <- generateRandomDag(numNodes, maxParents)
-arrThetas <- generateMultinomialParamsUsingWeightParameter(mAdj, cardinalities)
-mObs <- generateSamplesFromModel(mAdj, arrThetas, 100)
+arrThetas <- generateMultinomialParams(mAdj, cardinalities)
+mObs <- generateSamplesFromModel(mAdj, arrThetas, 2500)
